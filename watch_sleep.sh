d2390363823827a1879c21bdf52df7442d46576a
Julie Chrzanowski <jlc3467@rit.edu>
2026-05-24 17:10:40 -0400

    Added comments for what true/false meant in the context of dbus the sleep trigger.

--- watch_sleep.sh
+++ watch_sleep.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 dbus-monitor --system "type='signal',interface='org.freedesktop.login1.Manager',member=PrepareForSleep" | while read x; do
     case "$x" in
-        *"boolean false"*)
+        *"boolean false"*) # wake
             systemctl --user --no-block start sleep.target;
             sleep 0.5;
             systemctl --user --no-block stop  sleep.target;
             ;;
-        # *"boolean true"*)  systemctl --user --no-block start sleep.target;;
+        # *"boolean true"*) # sleep
+            # systemctl --user --no-block start sleep.target
+            # ;;
     esac
 done
