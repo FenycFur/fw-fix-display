32dfbcc53a24d7d959d18a4e8134a14fb0d1da28
Julie Chrzanowski <jlc3467@rit.edu>
2026-05-24 15:26:45 -0400

    no need for install.sh with makefile

--- install.sh
+++
@@ -1,4 +0,0 @@
-chmod +x ~/.config/systemd/user/watch_sleep.sh
-systemctl --user daemon-reload
-systemctl --user enable watch_sleep.service fix-display.service
-systemctl --user start watch_sleep.service
